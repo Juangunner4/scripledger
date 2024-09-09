76643110aa79e903f6b7778f541186eb138605b9
Juan Vazquez <juanje1019@gmail.com>
2024-09-09 16:53:05 -0400

    Logs for NodeService.java

--- src/main/java/com/scripledger/services/NodeService.java
+++ src/main/java/com/scripledger/services/NodeService.java
@@ -9,6 +9,7 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
 
 import java.util.Date;
 
@@ -22,17 +23,18 @@ public class NodeService {
     @RestClient
     NodeClient nodeClient;
 
+    private static final Logger LOGGER = Logger.getLogger(UserAccountService.class);
+
     public Uni<IssueCurrencyResponse> issueBusinessCurrency(IssueCurrencyRequest request) {
+        LOGGER.info("Issuing business currency with Initial Supply: " + request.getInitialSupply() + " and Decimals: " + request.getDecimals());
         return nodeClient.issueBusinessCurrency(request)
                 .onItem().invoke(response -> {
-                    // Create a new Transaction object
                     Transaction transaction = new Transaction();
                     transaction.setPublicKey(response.getMintPubKey());
                     transaction.setTransactionHash(response.getInitialSupplyTxnHash());
                     transaction.setTransactionType("issueBusinessCurrency");
                     transaction.setTimestamp(new Date());
-
-                    // Save the transaction to MongoDB
+                    LOGGER.info("Business mintPubKey: " + response.getMintPubKey());
                     transactionRepository.persist(transaction);
                 });
     }
